b3e4cece6d3c822b4a586bdc8faeea330e1598ed
yashpriyam <52121893+yashpriyam@users.noreply.github.com>
2021-02-23 00:53:06 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { useEffect, useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setTimeout(() => setTime(new Date()), 1000);
+    return () => clearTimeout(timer);
+  }, [time]);
+
+  // same as :
+  // useEffect(() => {
+  //   const timer = setTimeout(() => setTime(new Date()), 1000);
+  //   return () => clearTimeout(timer);
+  // });
   return (
     <div className="App">
-      testing for changes on github
+      <h2>{time.toLocaleTimeString()}</h2>
     </div>
   );
 }
