f008b8e2c91274e2864cde5b7df82f769f15ad96
yashpriyam <52121893+yashpriyam@users.noreply.github.com>
2021-02-23 07:39:32 +0530

    useContext hook with functional setState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import UseEffectApp from "../src/Hooks/useEffect";
+import { Parent } from "./Hooks/useContext";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       <UseEffectApp />
+      <Parent />
     </div>
   );
 }
---
+++ src/Hooks/useContext.js
@@ -0,0 +1,34 @@
+import { useState, createContext, useContext } from "react";
+
+const PropSetContext = createContext({
+  stateVar: "",
+  setState: () => {}
+});
+
+export const Parent = () => {
+  const [state, setState] = useState("string1");
+  return (
+    <PropSetContext.Provider value={{ stateVar: state, setState }}>
+      <Level1 />
+    </PropSetContext.Provider>
+  );
+};
+
+const Level1 = () => <Level2 />;
+
+const Level2 = () => <Level3 />;
+
+const Level3 = () => <Level4 />;
+
+const Level4 = () => <Level5 />;
+
+const Level5 = () => {
+  const { stateVar, setState } = useContext(PropSetContext);
+  return (
+    <h1
+      onClick={() => setState((x) => (x === "string1" ? "string2" : "string1"))}
+    >
+      {stateVar}
+    </h1>
+  );
+};
